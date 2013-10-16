6dd14e9a9256c208397537678dabb98c9db872da
Martin Nash <martin.j.nash@gmail.com>
2013-10-16 00:53:09 -0700

    Martin Nash did his homework

--- week2/exercises/book_spec.rb
+++ week2/exercises/book_spec.rb
@@ -4,11 +4,23 @@ describe Book do
 	before :each do
 		@book = Book.new("Harry Potter", 200)
 	end
-	it "should respond to title" do
-		@book.should respond_to "title"
+
+
+	# Group the title tests together
+	context "#title" do
+		it "should respond to title" do
+			@book.should respond_to "title"
+		end
 	end
+
+
+	it "should have a title" do
+		@book.should_not be_nil
+	end
+
 
 	it "should return the page count" do
 		@book.page_count.should eq "Page count is 200"
 	end
+
 end
