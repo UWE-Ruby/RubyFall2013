56b5391d44fc377f20c057397af3425390382cab
sdodson <sdodson@gettyimages.com>
2013-10-17 18:50:01 -0700

    commit cleanup defined methods in simon_says.rb
    answered questions 1-3
    
    answered questions 4 and 5 and worked on extending 2
    
    migration to rbenv and removal of rvm for work compatibility
    
    dding rbenv dot files
    
    cleanup
    
    cleanup
    
    cleanup
    
    answered week3 questions
    
    work on calculator

--- week2/homework/simon_says.rb
+++ week2/homework/simon_says.rb
@@ -1,20 +1,37 @@
-class SimonSays
+module SimonSays
 
-	#define methods
-	#echo - echo("string") repeat back a string
+	#define methods	echo, shout, repeat, start_of_word and first_word
+
+    #echo - echo("string") repeat back a string
+	def echo(string)
+		string
+	end
+
 	#shout - shout("string") convert a string to caps
-	#repeat - repeat("string", @num) repeat a string @num times
-	#start_of_word - start_of_word("string", @num) return @num characters of string
+	def shout(string)
+		string.upcase
+	end
+
+	#repeat - repeat("string", number) repeat a string number times
+	def repeat(string, n=2)
+		([string]*n).join(' ')
+	end
+
+	#start_of_word - start_of_word("string", number) return number characters of string
+	def start_of_word(string, number)
+		string[0,number]
+	end
+
 	#first_word - first_word("multi word string") return first word of string
-
-	attr_accessor :title, :pages
-
-	def initialize(title, pages)
-		@title = title
-		@pages = pages
+	def first_word(string)
+		#string.split(' ', 0) [0]
+		string.split(/ /, 0) [0]
 	end
 
-	def page_count
-		"Page count is #{@pages}"
+	#any_word - any_word("multi word string", number) return number word from string
+	def any_word(string, number)
+		# string.split(' ', 0) [number]
+		string.split(/ /, 0) [number]
 	end
+
 end
