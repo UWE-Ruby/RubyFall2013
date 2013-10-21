a7b8fd37de595ec9240413f3bb7f17fd6f98714d
paulsheridan <paulsheridan@Pauls-MacBook-Air.local>
2013-10-20 17:26:41 -0700

    Homework 2 Finished

---
+++ week2/homework/simon_says.rb
@@ -0,0 +1,22 @@
+module SimonSays
+	def echo (input = "")
+		input
+	end		
+
+	def shout (input = "")
+		input.upcase 
+	end	
+
+	def repeat (input = "", n = 2)
+		newString = ([input] * n).join(' ')
+		newString.strip
+	end	
+
+	def start_of_word (input, numberOfLetters)
+		input[0,numberOfLetters]
+	end	
+
+	def first_word (input = "")
+		input.split[0]
+	end
+end
