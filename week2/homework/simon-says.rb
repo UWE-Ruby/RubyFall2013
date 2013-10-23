24457e9dab5f157f8779117163bbe48615e66c35
Jameson Brown <jameson.brown@gmail.com>
2013-10-22 17:56:45 -0700

    Completed week 2 homework

---
+++ week2/homework/simon-says.rb
@@ -0,0 +1,22 @@
+module SimonSays
+	def echo(name)
+		return name
+	end
+
+	def shout(name)
+		return name.upcase
+	end
+
+	def repeat (name, n)
+		Array.new(n, name).join ' '
+	end
+
+	def start_of_word(name, n)
+		@frag = name.scan /\w/
+		return @frag[0...n].join ' '
+	end
+
+	def first_word(name)
+		return name.split(" ")[0]
+	end
+end
