433e2fe1e4e029bc1b1a14215a344c818bf1d15e
Jonas Haskins <h20melonman@gmail.com>
2013-10-24 13:51:50 -0700

    my book exe

--- week2/exercises/book.rb
+++ week2/exercises/book.rb
@@ -1,13 +1,28 @@
 class Book
+	attr_accessor :title
+	attr_reader :page_count
+	#attr_reader :title #reader only ? 
+	@@book_count = 0
+	
+	#self. = class level 
+	def self.book.count
+		## @@ = class level varable
+		@@book_count 
+	end
 
-	attr_accessor :title, :pages
-
-	def initialize(title, pages)
+	def initalize title = "Not Set", page_count = 0
+		@@book_count += 1
+		@page_count = page_count	
 		@title = title
-		@pages = pages
 	end
 
-	def page_count
-		"Page count is #{@pages}"
+	def test
+		@test = "Hellow!"
 	end
+
+	def out_put_test
+		puts @test
+		puts @@book_count
+	end
+
 end
