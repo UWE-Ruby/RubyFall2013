e3a1a276e2c6b55acb86ccc0c70d594bb275c11a
wmuengineer <engineerbell@gmail.com>
2013-10-24 23:11:42 -0700

    Calculation.rb is complete

--- week2/exercises/book.rb
+++ week2/exercises/book.rb
@@ -1,18 +1,41 @@
 class Book
+  attr_accessor :title
+  attr_reader :page_count
 
-	attr_accessor :title, :pages
+  @@book_count = 0
 
+<<<<<<< HEAD
   def self.library_count
     @@book_count
   end
 
   def test
     @test= "hello!"
+=======
+  def self.book_count
+    @@book_count
+  end
+
+  def initialize title = "Not Set", page_count = 0
+    @@book_count += 1
+    @page_count = page_count
+    @title = title
+  end
+
+
+  def test
+    @test = "Hello!"
+>>>>>>> fde8a74b427aa79cd14a4014401682c12225e82b
   end
 
   def out_put_test
     puts @test
     puts @@book_count
   end
+<<<<<<< HEAD
 end
 
+=======
+
+end
+>>>>>>> fde8a74b427aa79cd14a4014401682c12225e82b
--- week2/exercises/book_spec.rb
+++ week2/exercises/book_spec.rb
@@ -1,6 +1,7 @@
-require './book.rb'
+require './book'
 
 describe Book do
+<<<<<<< HEAD
 	
 	context "::new" do
 	  it ""
@@ -28,3 +29,51 @@ describe Book do
 end
 end
 
+=======
+
+  
+  context "::book_count" do
+
+    it "should count how many books have been created" do
+      Book.new
+      Book.new
+      Book.new
+      Book.book_count.should eq 3
+    end
+
+  end
+
+  context "::new" do
+
+    it "should set some defaults" do
+      Book.new.title.should eq "Not Set"
+    end
+
+    it "should allow us to set the page count" do
+      book = Book.new "Harry Potter", 5
+      book.page_count.should eq 5
+    end
+
+  end
+  
+  context "#title" do
+
+    before :each do
+      @book = Book.new
+    end
+
+    it "should have a title" do
+      @book.should respond_to "title"
+    end
+
+    it "should allow me to set the title" do
+      @book.title = "Snow Crash"
+      @book.title.should eq "Snow Crash"
+    end
+
+
+
+  end
+
+end
+>>>>>>> fde8a74b427aa79cd14a4014401682c12225e82b
