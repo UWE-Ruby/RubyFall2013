d1b07e4805dea6e800706a0ef9f8d403095eb443
sdodson <sdodson@gettyimages.com>
2013-10-29 15:21:45 -0700

    cleanup

--- week3/homework/calculator.rb
+++ week3/homework/calculator.rb
@@ -1,20 +1,20 @@
 class Calculator
 
 	def sum(numbers)
-		numbers.inject(0){|sum, a| sum +a}
+	  numbers.inject(0){|sum, a| sum +a}
 	end
 
 	def multiply(*numbers)
-        numbers.flatten.inject(:*)
+	  numbers.flatten.inject(:*)
 	end
 	
 	def pow(base, power)
-		base**power
+	  base**power
 	end
 
-	def fac(n) 
-        return 1 if n.zero?
-        1.upto(n).inject(:*)
-    end
+	def fac(n)
+	  return 1 if n.zero?
+	  1.upto(n).inject(:*)
+	end
 
 end
