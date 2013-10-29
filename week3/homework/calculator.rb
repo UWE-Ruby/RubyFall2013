8994f324cfa95516cf24b4364eb847634b2ef4ef
sdodson <sdodson@gettyimages.com>
2013-10-29 15:18:24 -0700

    week3 homework

--- week3/homework/calculator.rb
+++ week3/homework/calculator.rb
@@ -1,26 +1,18 @@
 class Calculator
 
-	# computes sum of two or more numbers
 	def sum(numbers)
-		#numbers.inject(:+)
 		numbers.inject(0){|sum, a| sum +a}
 	end
 
-    # multiplies two numbers or an array of numbers
-	 def multiply(*numbers)
-	 	#numbers.inject(:*)
-	 	#numbers.inject(1){|product, n| product * n}
-	 	#numbers.reject(&:zero?).inject(:*)
+	def multiply(*numbers)
         numbers.flatten.inject(:*)
-	 end
-
-	# raises one number to the power of another
+	end
+	
 	def pow(base, power)
 		base**power
 	end
 
-    # http://en.wikipedia.org/wiki/Factorial
-	def fac(n) # http://www.rodrigoalvesvieira.com/the-most-beautiful-factorial-method/
+	def fac(n) 
         return 1 if n.zero?
         1.upto(n).inject(:*)
     end
