c443d31ddf36e55539d497954716663f87164e4f
Adam Hendricksen <adam@nird.us>
2013-10-28 21:35:14 -0700

    All those parenthesis are just unneeded punctuation

--- week3/homework/calculator.rb
+++ week3/homework/calculator.rb
@@ -1,24 +1,24 @@
 class Calculator
-  def pow(base, exp)
-    values = Array.new(exp, base)
-    repeated_operation(values, 1, :*)
+  def pow base, exp
+    values = Array.new exp, base
+    repeated_operation values, 1, :*
   end
 
-  def sum(*values)
-    repeated_operation(values, 0, :+)
+  def sum *values
+    repeated_operation values, 0, :+
   end
 
-  def multiply(*values)
-    repeated_operation(values, 1, :*)
+  def multiply *values
+    repeated_operation values, 1, :*
   end
 
-  def fac(value)
+  def fac value
     values = value.downto(1).to_a
-    repeated_operation(values, 1, :*)
+    repeated_operation values, 1, :*
   end
 
   private
-  def repeated_operation(array, start, operator)
+  def repeated_operation array, start, operator
     array.flatten!
     array.inject start, operator
   end
