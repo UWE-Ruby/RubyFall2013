6b5ba444810862cc7beff4fb61c758a1d7a3e114
sdodson <sdodson@gettyimages.com>
2013-10-29 18:43:36 -0700

    class notes

--- week3/homework/calculator.rb
+++ week3/homework/calculator.rb
@@ -1,7 +1,8 @@
 class Calculator
 
 	def sum(numbers)
-	  numbers.inject(0){|sum, a| sum +a}
+	  # numbers.inject(0){|sum, a| sum +a}
+	  numbers.inject(0, :+) #from class
 	end
 
 	def multiply(*numbers)
@@ -15,6 +16,7 @@ class Calculator
 	def fac(n)
 	  return 1 if n.zero?
 	  1.upto(n).inject(:*)
+	  # (1..n).inject(1, :+) ??
 	end
 
 end
