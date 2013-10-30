7ab4c2f4cecca3d583c37fc79df1f070389d86c0
Karen Kirr <kkirr77@yahoo.com>
2013-10-29 18:00:26 -0700

    class updates

--- week2/exercises/book.rb
+++ week2/exercises/book.rb
@@ -2,7 +2,7 @@ class Book
 
 	attr_accessor :title, :pages
 
-	def initialize(title, pages)
+	def initialize (title, pages)
 		@title = title
 		@pages = pages
 	end
--- week2/exercises/book_spec.rb
+++ week2/exercises/book_spec.rb
@@ -1,6 +1,18 @@
 require './book.rb'
 
 describe Book do
+
+#	context "::new" do
+
+	#it "should set some defaults" do 
+	#	Book.new.title.should eq "Not Set"
+	#end
+#end
+
+it "should return the page count" do
+		@book.page_count.should eq "Page count is 200"
+	end
+
 	before :each do
 		@book = Book.new("Harry Potter", 200)
 	end
@@ -8,7 +20,13 @@ describe Book do
 		@book.should respond_to "title"
 	end
 
-	it "should return the page count" do
-		@book.page_count.should eq "Page count is 200"
-	end
+	#context "#title" do
+
+	#it "should allow me to set the title" do 
+	#	@book.title = "Snow Crash"
+	#	@book.title should eq "Snow Crash"
+	#	end
+
+	#end
+	
 end
