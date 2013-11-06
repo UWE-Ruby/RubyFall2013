c555d584b14b477fe355223bda8d1b7a015ccef5
Martin Nash <martin.j.nash@gmail.com>
2013-11-05 20:28:36 -0800

    added other student examples to worker file

--- week4/exercises/worker.rb
+++ week4/exercises/worker.rb
@@ -1,9 +1,21 @@
 module Worker
 
   def self.work (rounds=1, &block)
-    result = nil
-    rounds.times { result = yield }
-    result
+
+    # I'm doing an acumulation
+    # result = nil
+    # rounds.times { result = yield }
+    # result
+
+    # CRAIG's SOLUTION
+    # (0..rounds).inject {yield}
+
+    # Renée's solution
+    rounds.times.inject(nil){ yield }
+
+    # Whenever you have an accumulator, you should look at inject
   end
+
+
 
 end
