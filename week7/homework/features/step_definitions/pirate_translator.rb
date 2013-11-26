b9565878123f5a91b4a905f733e71bf51e16e17d
Craig Adams <craigad1@uw.edu>
2013-11-25 20:04:09 -0800

    answered questions and got pirate features to pass

---
+++ week7/homework/features/step_definitions/pirate_translator.rb
@@ -0,0 +1,14 @@
+class PirateTranslator
+
+  def initialize
+      @pirate_string = "Ahoy Matey\n Shiber Me Timbers You Scurvey Dogs!!"
+  end
+
+  def say(args)
+    return args
+  end
+
+  def translate
+    return @pirate_string
+  end
+end
