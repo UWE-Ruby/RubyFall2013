1e6b09525254e61db78314a5e98a26088c64a743
Martin Nash <martin.j.nash@gmail.com>
2013-11-26 18:17:04 -0800

    removed final project files from week7

--- week4/exercises/code_timer.rb
+++
@@ -1,9 +0,0 @@
-class CodeTimer
-
-  def self.time_code n=1
-    start_time = Time.now
-    n.times { yield } 
-    (Time.now - start_time) / n
-  end
-
-end
--- week4/exercises/person.rb
+++
@@ -1,9 +0,0 @@
-require './named_thing'
-
-class Person
-  include NamedThing
-
-  def shout_name
-    "MY NAME IS #{@name.upcase}"
-  end
-end
