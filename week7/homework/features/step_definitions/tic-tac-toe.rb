32a664a31514fc36401b099dd7d20457cdcc1baf
Alicia French <alienor18@yahoo.com>
2013-12-01 23:34:01 -0800

    second version of tic-tac-toe with correct over? function and check of player's input

--- week7/homework/features/step_definitions/tic-tac-toe.rb
+++ week7/homework/features/step_definitions/tic-tac-toe.rb
@@ -16,7 +16,7 @@ class TicTacToe
     @open_spots = [:A1,:A2,:A3,:B1,:B2,:B3,:C1,:C2,:C3]
     @player_won = false
     @computer_won = false
-    @draw = true
+    @draw = false
   end
 
   def welcome_player
@@ -43,6 +43,18 @@ class TicTacToe
 
   def player_move
     pMove = gets.chomp
+    valid = false
+    until  valid == true
+      if @board.keys.include?(pMove.to_sym)==false
+       puts "Invalid input!"
+       pMove = gets.chomp
+      elsif @open_spots.include?(pMove.to_sym)==false
+       puts "Spot already taken!"
+       pMove = gets.chomp
+      else 
+        valid = true
+      end
+    end
     @board[pMove.to_sym]=@player_symbol
     @open_spots.delete(pMove.to_sym)
     @current_player="Computer"   
@@ -53,11 +65,26 @@ class TicTacToe
   end
 
   def over?
-    if @open_spots.empty?
-      @over = true
+    #detect 3 consecutives symbols
+    if (@board[:A1]==@board[:A2] && @board[:A1]==@board[:A3] && @board[:A1]=="X" ) || (@board[:B1]==@board[:B2] && @board[:B1]==@board[:B3] && @board[:B1]=="X" ) || (@board[:C1]==@board[:C2] && @board[:C1]==@board[:C3] && @board[:C1]=="X" ) || (@board[:A1]==@board[:B1] && @board[:A1]==@board[:C1] && @board[:A1]=="X" ) || (@board[:A2]==@board[:B2] && @board[:A2]==@board[:C2] && @board[:A2]=="X" ) || (@board[:A3]==@board[:B3] && @board[:A3]==@board[:C3] && @board[:A3]=="X" ) || (@board[:A1]==@board[:B2] && @board[:A1]==@board[:C3] && @board[:A1]=="X" ) || (@board[:A3]==@board[:B2] && @board[:A3]==@board[:C1] && @board[:A3]=="X" )
+       if @player_symbol == "X"
+          @player_won = true
+       elsif @computer_symbol == "X"
+          @computer_won = true
+       end
+       @over = true
+    elsif (@board[:A1]==@board[:A2] && @board[:A1]==@board[:A3] && @board[:A1]=="O" ) || (@board[:B1]==@board[:B2] && @board[:B1]==@board[:B3] && @board[:B1]=="O" ) || (@board[:C1]==@board[:C2] && @board[:C1]==@board[:C3] && @board[:C1]=="O" ) || (@board[:A1]==@board[:B1] && @board[:A1]==@board[:C1] && @board[:A1]=="O" ) || (@board[:A2]==@board[:B2] && @board[:A2]==@board[:C2] && @board[:A2]=="O" ) || (@board[:A3]==@board[:B3] && @board[:A3]==@board[:C3] && @board[:A3]=="O" ) || (@board[:A1]==@board[:B2] && @board[:A1]==@board[:C3] && @board[:A1]=="O" ) ||
+ (@board[:A3]==@board[:B2] && @board[:A3]==@board[:C1] && @board[:A3]=="O" )
+       if @player_symbol == "O"
+          @player_won = true
+       elsif @computer_symbol == "O"
+          @computer_won = true
+       end
+       @over = true
+    elsif @open_spots.empty?
+       @draw = true
+       @over = true
     end
-    puts @open_spots
-    puts "\n "
     @over
   end
 
