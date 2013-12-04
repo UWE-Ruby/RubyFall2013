211028b1e59795c465d9ef08ad0c2f00da1d4ed0
Adam Hendricksen <adam@nird.us>
2013-12-03 20:31:56 -0800

    I think I need to keep track of the current player

--- week7/homework/features/tic-tac-toe.rb
+++ week7/homework/features/tic-tac-toe.rb
@@ -1,13 +1,14 @@
 class TicTacToe
   attr_accessor :player, :player_symbol, :computer_symbol
   SYMBOLS = @player_symbol, @computer_symbol
+  @current
 
   def welcome_player
     "Welcome #{@player}"
   end
 
   def current_player
-    [@player, "Computer"].sample
+    @current = [@player, "Computer"].sample
   end
 
   def initialize *name
