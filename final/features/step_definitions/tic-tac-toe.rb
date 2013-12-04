c93beb507fba79d4f9605464e5022b0cd447effc
Sean Sehr <sean@seansehr.com>
2013-12-03 16:29:18 -0800

    getting Scenario: My Turn passing

--- final/features/step_definitions/tic-tac-toe.rb
+++ final/features/step_definitions/tic-tac-toe.rb
@@ -1,22 +1,69 @@
 class TicTacToe
-  attr_accessor :player
-  attr_reader :current_player, :player_symbol, :computer_symbol
+  attr_accessor :board, :player
+  attr_reader :player_symbol, :computer_symbol
   SYMBOLS = ["X", "O"]
 
-  def initialize player
-    @player = "Player"
-    @current_player = @current_player || [@player, "Computer"].sample
-    if @current_player == "Computer"
+  def initialize player = "Player"
+    @player = player
+
+    @computers_turn = rand() > 0.5 ? true : false
+    if @computers_turn
       @computer_symbol = SYMBOLS[0]
       @player_symbol = SYMBOLS[1]
     else
       @computer_symbol = SYMBOLS[1]
       @player_symbol = SYMBOLS[0]
     end
+
+    @board = {
+      :A1 => false, :A2 => false, :A3 => false,
+      :B1 => false, :B2 => false, :B3 => false,
+      :C1 => false, :C2 => false, :C3 => false
+    }
   end
 
   def welcome_player
     "Welcome #{@player}"
   end
 
+  def current_player
+    @computers_turn ? "Computer" : @player
+  end
+
+  def computer_move
+    spot = computer_find_spot
+    move(spot)
+  end
+
+  def computer_find_spot
+    find_empty_spots.sample
+  end
+
+  def indicate_player_turn
+    "#{@player}'s Move:"
+  end
+
+  def get_player_move
+    STDOUT.flush
+    input = gets.chomp.upcase.to_sym
+    move(input)
+  end
+
+  def find_empty_spots
+    spots = []
+    @board.each{|k, v| spots << k if v == false }
+    spots
+  end
+
+  def move spot
+    if @board[spot] == false
+      symbol = @computers_turn ? @computer_symbol : @player_symbol
+      @board[spot] = symbol
+      next_turn
+    end
+  end
+
+  def next_turn
+    @computers_turn = !@computers_turn
+  end
 end
