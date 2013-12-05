085ff69595510267280c7cbad65999d4386eaeb2
Adam Hendricksen <adam@nird.us>
2013-12-05 14:17:40 -0800

    I'm shooting in the dark, no idea what I'm hitting...

--- week7/homework/features/tic-tac-toe.rb
+++ week7/homework/features/tic-tac-toe.rb
@@ -1,10 +1,7 @@
 class TicTacToe
-  attr_accessor :player, :player_symbol, :computer_symbol
+  attr_accessor :player, :computer, :player_symbol, :computer_symbol
   SYMBOLS = @player_symbol, @computer_symbol
-  @computer = "computer"
   @current
-  @player_symbol
-  @computer_symbol
 
   def welcome_player
     "Welcome #{@player}"
@@ -20,14 +17,9 @@ class TicTacToe
   end
 
   def initialize *name
-    @current = [@player, @computer].sample unless name[0]
-    if name[1] == 'X'
-      @player_symbol = :O
-      @computer_symbol = :X
-    else
-      @player_symbol = :X
-      @computer_symbol = :O
-    end
+    symbols = [:X, :O]
+    name[1] ||= symbols.sample
+    @player_symbol = symbols.fetch(symbols.index(name[1]))
   end
 
   def indicate_player_turn
