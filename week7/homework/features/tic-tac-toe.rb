601503058e024c9d67c0a749816adf0dc1c80921
Adam Hendricksen <adam@nird.us>
2013-12-05 13:11:57 -0800

    Turns must alternate

--- week7/homework/features/tic-tac-toe.rb
+++ week7/homework/features/tic-tac-toe.rb
@@ -1,16 +1,19 @@
 class TicTacToe
   attr_accessor :player, :player_symbol, :computer_symbol
   SYMBOLS = @player_symbol, @computer_symbol
-  @current
+  @current = "Computer"
 
   def welcome_player
     "Welcome #{@player}"
   end
 
   def current_player
-    @current = [@player, "Computer"].sample
+    @current = @player if @current == "Computer"
+    @current = "Computer" if @current == @player
+    @current
   end
 
   def initialize *name
+    @current = [@player, "Computer"].sample unless name[0]
   end
 end
