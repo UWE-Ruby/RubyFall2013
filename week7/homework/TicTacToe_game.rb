4ac1ce0535ab8e711ddf896c1a2eafb34c40970f
wmuengineer <engineerbell@gmail.com>
2013-12-09 20:20:07 -0800

    Added current_state method.

--- week7/homework/TicTacToe_game.rb
+++ week7/homework/TicTacToe_game.rb
@@ -1,19 +1,18 @@
 class TicTacToe
 	SYMBOLS = [:X,:O]
-
 	attr_accessor :player
 	attr_accessor :player_on_deck
 	attr_accessor :current_player
+  attr_accessor :location
 	attr_reader :welcome_player
 	attr_reader :player_symbol
 	attr_reader :computer_symbol
 	attr_accessor :board
 	attr_reader :first
   attr_accessor :board_locations
-	
+  attr_accessor :board
+
 	def initialize (name = " ",symbol = :X )
-
-
     @winning_patterns = [
 
         [:A1,:A2,:A3],
@@ -23,21 +22,18 @@ class TicTacToe
         [:A2,:B2,:C2],
         [:A3,:B3,:C3],
         [:A1,:B2,:C3],
-        [:A3,:B2,:C1],
+        [:A3,:B2,:C1]
 
     ]
 
+
+
+    @board_locations = {:A1 =>" ", :A2 =>" ", :A3 =>" ",
+                       :B1 =>" ", :B2 =>" ", :B3 =>" ",
+                       :C1 =>" ", :C2 =>" ", :C3 =>" "}
 	@move_counter = 0
-
-
   @player_symbol = symbol
-
   @player_symbol == :X ? @computer_symbol =:O :  @computer_symbol =:X
-
-
-
-
-  				
 	end
 
 	def player
@@ -48,12 +44,11 @@ class TicTacToe
 		@welcome_player =  "Welcome #{@player}"
 	end
 
-	def indicate_player_turn 
+	def indicate_player_turn
 		puts "#{@player}'s Move:"
-
 	end
 
-	def current_player
+  def current_player
 
     #if @move_counter == 0
     #  player_on_deck = rand() > 0.5 ? "Computer" : @player
@@ -70,7 +65,7 @@ class TicTacToe
 
     (player_on_deck == @player) ? symbol = @player_symbol : symbol = @computer_symbol
 
-    return @player_on_deck ; symbol
+   @player_on_deck
 	end
 
 	def get_move
@@ -90,61 +85,69 @@ class TicTacToe
 	end
 
 	def get_player_move
-  	location = gets.chomp.to_sym
+  	@location = gets.chomp.to_sym
     #board_locations[location] = player_symbol
   	#computer_move
 
-		
+
 	end
 
-	def board (*args)
-     board_locations = {:A1 => " ", :A2 => " ", :A3 => " ",
-         :B1 => " ", :B2 => " ", :B3 => " ",
-         :C1 => " ", :C2 => " ", :C3 => " ",
-     }
+	def board
 
-     board_locations[get_player_move] = player_symbol
-    
-    
-	#puts "-------------------"
-	#puts "#{@player}"	
+    Proc.new do |get_player_move = :B2|
+      board_value = get_player_move
+      @board_locations[board_value]
 
-	#puts "   1| 2 | 3\n"
-	puts " A| #{board_locations[:A1]} | #{board_locations[:A2]} | #{board_locations[:A3]} "
-	#puts " ------------------"
-	puts " B| #{board_locations[:B1]} | #{board_locations[:B2]} | #{board_locations[:B3]} "
-	#puts " ----------------------------"
-	puts " C| #{board_locations[:C1]} | #{board_locations[:C2]} | #{board_locations[:C3]} "
-	#puts " ------------------"
-		
+
+      ##puts "-------------------"
+      ##puts "#{@player}"
+      #
+      ##puts "   1| 2 | 3\n"
+      #puts " A| #{@board_locations[:A1]} | #{@board_locations[:A2]} | #{@board_locations[:A3]} "
+      ##puts " ------------------"
+      #puts " B| #{@board_locations[:B1]} | #{@board_locations[:B2]} | #{@board_locations[:B3]} "
+      ##puts " ----------------------------"
+      #puts " C| #{@board_locations[:C1]} | #{@board_locations[:C2]} | #{@board_locations[:C3]} "
+      ##puts " ------------------"
+      #
+
+    end
+
 	end
 
 	def open_spots
+    @board_locations.has_value?(" ")
+
 	end
 
 	def computer_move
 	end
 
-	def player_move	
+	def player_move
+    get_player_move.to_sym
 	end
 
 	def current_state
+    @board_locations[get_player_move.to_sym] = @player_symbol.to_s
+    @board_locations[get_player_move.to_sym]
 	end
 
 	def player_won?
-		
+
 	end
 
 	def over?
-		
+
 	end
 
 	def draw?
-		
+
 	end
 
 	def determine_winner
-		
-	end
+
+  end
 
 end
+
+
